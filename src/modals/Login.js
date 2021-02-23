f33469369da9977c3a8d41487ab3284f3a4d6f85
wizebin <wizebin@gmail.com>
2021-02-22 20:42:21 -0500

    Removed fake default host and password

--- src/modals/Login.js
+++ src/modals/Login.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, View } from 'react-native';
 import Button from '../components/Button';
 import Input from '../components/Input';
 
 export default function Login(props) {
-  const { onDismiss, credentials, onSave } = props;
-  const [host, setHost] = useState(credentials?.host || '192.168.1.107:9434');
-  const [password, setPassword] = useState(credentials?.password || 'wizebin');
+  const { credentials, onSave } = props;
+  const [host, setHost] = useState(credentials?.host);
+  const [password, setPassword] = useState(credentials?.password);
 
   return (
     <View>
-      <Text style={{ fontSize: 22, fontWeight: '200', color: '#fff' }}>Enter your OBS creds</Text>
-      <Text style={{ fontSize: 14, marginBottom: 30, color: '#fff' }}>These are you obs-websocket credentials</Text>
-      <Input style={{ color: '#fff' }} value={host} onChangeText={setHost} placeholder="host"></Input>
+      <Text style={{ fontSize: 22, fontWeight: '200', color: '#fff' }}>OBS-Websocket Credentials</Text>
+      <Text style={{ fontSize: 14, marginBottom: 30, color: '#fff' }}>Enter the computer ip running OBS, and the port/password you specified in the obs-websocket plugin</Text>
+      <Input style={{ color: '#fff' }} value={host} onChangeText={setHost} placeholder="host (eg. 192.168.0.2:4444)"></Input>
       <Input style={{ color: '#fff' }} secureTextEntry textContentType="password" value={password} onChangeText={setPassword} placeholder="password"></Input>
 
       <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginTop: 12 }}>
